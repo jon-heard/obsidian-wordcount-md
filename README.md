539e0b4514e2b2f1a778006371468cb325d90a1b
Jon Heard <jon.heard.tutor@gmail.com>
2022-11-11 03:33:41 -0500

    - Added finalized plugin to the repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Word Count - markdown oriented
+
+Counts the NON-MARKDOWN words of selected text in the editor.
+
+This word count plugin is different in that it considers a word as any non-spaced character string with AT LEAST ONE alpha-numeric character.  This is important, because markdown can include A LOT of non-word text, which traditional word counters will display with mis-leadingly large word counts.
+
---
+++ main.js
@@ -0,0 +1,62 @@
+// WordCountMd - A plugin to count the NON-MARKDOWN words of selected text in the editor.
+
+'use strict';
+
+var obsidian = require('obsidian');
+var state = require('@codemirror/state');
+var view = require('@codemirror/view');
+
+class WordCountMd extends obsidian.Plugin
+{
+	onload()
+	{
+		BetterWordCount.statusBar = this.addStatusBarItem();
+		this.createCMExtension();
+	}
+
+	createCMExtension()
+	{
+		const cmStateField = state.StateField.define(
+		{
+			create(state)
+			{
+				return view.Decoration.none;
+			},
+			update(effects, tr)
+			{
+				let text = "";
+				const selection = tr.newSelection.main;
+				if (selection.empty)
+				{
+					const textIter = tr.newDoc.iter();
+					while (!textIter.done)
+					{
+						text = text + textIter.next().value;
+					}
+				}
+				else
+				{
+					const textIter = tr.newDoc.iterRange(selection.from, selection.to);
+					while (!textIter.done)
+					{
+						text = text + textIter.next().value;
+					}
+				}
+				BetterWordCount.updateStatusBar(text);
+				return effects;
+			},
+			provide: (f) => view.EditorView.decorations.from(f),
+		});
+
+		this.registerEditorExtension(cmStateField);
+	}
+
+	static updateStatusBar(text)
+	{
+		const words = text.match(/[^\s/]*([a-zA-Z0-9])[^\s/]*/g).length;
+		const chars = text.length;
+		BetterWordCount.statusBar.setText(`${words} words ${chars} characters`);
+	}
+}
+
+module.exports = BetterWordCount;
